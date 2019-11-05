ea5f3d1306d6dd0d3cd317217463e40f368bcbfe
gcnyin <guchounongyin@gmail.com>
2019-11-05 13:11:08 +0800

    add name property

--- mini-parser/index.ts
+++ mini-parser/index.ts
@@ -1,17 +1,19 @@
 interface IRule {
+    getName(): string | null | undefined;
     accept(input: string): { contain: boolean, groups: IGroup[] };
 }
 
 interface IGroup {
+    getName(): string | null | undefined;
     getGroups(): string | IGroup[];
     toRawString(): string;
 }
 
 class TreeGroup implements IGroup {
-    protected constructor(private readonly groups: IGroup[]) { }
+    protected constructor(private readonly groups: IGroup[], private readonly name: string | null | undefined = null) { }
 
-    public static of(...groups: IGroup[]) {
-        return new TreeGroup(groups);
+    public static of(groups: IGroup[], name: string | null | undefined = null) {
+        return new TreeGroup(groups, name);
     }
 
     public getGroups() {
@@ -21,13 +23,17 @@ class TreeGroup implements IGroup {
     public toRawString() {
         return this.groups.map(g => g.toRawString()).join("");
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class LeafGroup implements IGroup {
-    protected constructor(private readonly text: string) { }
+    protected constructor(private readonly text: string, private readonly name: string | null | undefined = null) { }
 
-    public static of(text: string) {
-        return new LeafGroup(text);
+    public static of(text: string, name: string | null | undefined = null) {
+        return new LeafGroup(text, name);
     }
 
     public getGroups() {
@@ -37,28 +43,36 @@ class LeafGroup implements IGroup {
     public toRawString() {
         return this.text;
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class RawTextRule implements IRule {
-    protected constructor(private readonly text: string) { }
+    protected constructor(private readonly text: string, private name: string | null | undefined = null) { }
 
-    public static of(text: string): IRule {
-        return new RawTextRule(text);
+    public static of(text: string, name: string | null | undefined = null): IRule {
+        return new RawTextRule(text, name);
     }
 
     public accept(input: string) {
         return { contain: input.startsWith(this.text), groups: [LeafGroup.of(this.text)] };
+    }
+
+    public getName() {
+        return this.name;
     }
 }
 
 class RegExpRule implements IRule {
     private readonly regExp: RegExp;
 
-    protected constructor(regExp: string | RegExp) {
+    protected constructor(regExp: string | RegExp, private readonly name: string | null | undefined = null) {
         this.regExp = new RegExp(regExp);
     }
 
-    public static of(regExp: string | RegExp): IRule {
+    public static of(regExp: string | RegExp, name: string | null | undefined = null): IRule {
         return new RegExpRule(regExp);
     }
 
@@ -66,20 +80,24 @@ class RegExpRule implements IRule {
         const regExpExecArray = this.regExp.exec(input);
         const contain = regExpExecArray ? regExpExecArray.index === 0 : false;
         const capturedText = regExpExecArray ? regExpExecArray[0] : '';
-        return { contain, groups: [LeafGroup.of(capturedText)] };
+        return { contain, groups: [LeafGroup.of(capturedText, this.name)] };
+    }
+
+    public getName() {
+        return this.name;
     }
 }
 
 class TimesRule implements IRule {
-    protected constructor(private readonly times: number, private readonly rule: IRule) { }
+    protected constructor(private readonly times: number, private readonly rule: IRule, private readonly name: string | null | undefined = null) { }
 
     /**
      * times should >= 0
      * if you pass a number that < 0
      * rule will accept it as 0
      */
-    public static of(times: number, rule: IRule): IRule {
-        return new TimesRule(times, rule);
+    public static of(times: number, rule: IRule, name: string | null | undefined = null): IRule {
+        return new TimesRule(times, rule, name);
     }
 
     public accept(input: string) {
@@ -89,7 +107,7 @@ class TimesRule implements IRule {
         for (; i < this.times; i++) {
             const { contain: _c, groups: _g } = this.rule.accept(temp);
             if (_c) {
-                groups.push(TreeGroup.of(..._g));
+                groups.push(TreeGroup.of(_g, this.name));
                 temp = temp.replace(_g.map(g => g.toRawString()).join(""), '');
             } else {
                 break;
@@ -98,13 +116,17 @@ class TimesRule implements IRule {
         const contain = this.times <= i;
         return { contain, groups };
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class OneOrMoreRule implements IRule {
-    protected constructor(private readonly rule: IRule) { }
+    protected constructor(private readonly rule: IRule, private readonly name: string | null | undefined = null) { }
 
-    public static of(rule: IRule): IRule {
-        return new OneOrMoreRule(rule);
+    public static of(rule: IRule, name: string | null | undefined = null): IRule {
+        return new OneOrMoreRule(rule, name);
     }
 
     public accept(input: string) {
@@ -114,7 +136,7 @@ class OneOrMoreRule implements IRule {
         for (; temp !== ''; count++) {
             const { contain: _c, groups: _g } = this.rule.accept(temp);
             if (_c) {
-                groups.push(TreeGroup.of(..._g));
+                groups.push(TreeGroup.of(_g, this.name));
                 temp = temp.replace(_g.map(g => g.toRawString()).join(""), '');
             } else {
                 break;
@@ -123,13 +145,17 @@ class OneOrMoreRule implements IRule {
         const contain = count > 0;
         return { contain, groups };
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class ZeroOrMoreRule implements IRule {
-    protected constructor(private readonly rule: IRule) { }
+    protected constructor(private readonly rule: IRule, private readonly name: string | null | undefined = null) { }
 
-    public static of(rule: IRule): IRule {
-        return new ZeroOrMoreRule(rule);
+    public static of(rule: IRule, name: string | null | undefined = null): IRule {
+        return new ZeroOrMoreRule(rule, name);
     }
 
     public accept(input: string) {
@@ -139,7 +165,7 @@ class ZeroOrMoreRule implements IRule {
         for (; temp !== '' ; count++) {
             const { contain: _c, groups: _g } = this.rule.accept(temp);
             if (_c) {
-                groups.push(TreeGroup.of(..._g));
+                groups.push(TreeGroup.of(_g, this.name));
                 temp = temp.replace(_g.map(g => g.toRawString()).join(""), '');
             } else {
                 break;
@@ -147,13 +173,17 @@ class ZeroOrMoreRule implements IRule {
         }
         return { contain: true, groups };
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class AndRule implements IRule {
-    protected constructor(private readonly rules: IRule[]) { }
+    protected constructor(private readonly rules: IRule[], private readonly name: string | null | undefined = null) { }
 
-    public static of(...rules: IRule[]) {
-        return new AndRule(rules);
+    public static of(rules: IRule[], name: string | null | undefined = null) {
+        return new AndRule(rules, name);
     }
 
     public accept(input: string) {
@@ -163,7 +193,7 @@ class AndRule implements IRule {
         for (let rule of this.rules) {
             const { contain, groups: _g } = rule.accept(temp);
             if (contain) {
-                groups.push(TreeGroup.of(..._g));
+                groups.push(TreeGroup.of(_g, this.name));
                 temp = temp.replace(_g.map(g => g.toRawString()).join(""), '');
                 count++;
             } else {
@@ -173,13 +203,17 @@ class AndRule implements IRule {
         const contain = count === this.rules.length;
         return { contain, groups };
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class OrRule implements IRule {
-    protected constructor(private readonly rules: IRule[]) { }
+    protected constructor(private readonly rules: IRule[], private readonly name: string | null | undefined = null) { }
 
-    public static of(...rules: IRule[]) {
-        return new OrRule(rules);
+    public static of(rules: IRule[], name: string | null | undefined = null) {
+        return new OrRule(rules, name);
     }
 
     public accept(input: string) {
@@ -189,7 +223,7 @@ class OrRule implements IRule {
         for (let rule of this.rules) {
             const { contain: _c, groups: _g } = rule.accept(temp);
             if (_c) {
-                groups.push(TreeGroup.of(..._g));
+                groups.push(TreeGroup.of(_g, this.name));
                 contain = true;
                 break;
             } else {
@@ -198,19 +232,27 @@ class OrRule implements IRule {
         }
         return { contain, groups };
     }
+
+    public getName() {
+        return this.name;
+    }
 }
 
 class AnyCharRule implements IRule {
-    protected constructor() { }
+    protected constructor(private readonly name: string | null | undefined = null) { }
 
-    public static of() {
-        return new AnyCharRule();
+    public static of(name: string | null | undefined = null) {
+        return new AnyCharRule(name);
     }
 
     public accept(input: string) {
         const contain = input.length > 0;
-        const groups: IGroup[] = contain ? [LeafGroup.of(input[0])] : [];
+        const groups: IGroup[] = contain ? [LeafGroup.of(input[0], this.name)] : [];
         return { contain, groups };
+    }
+
+    public getName() {
+        return this.name;
     }
 }
 
@@ -223,11 +265,15 @@ console.log(rule2.accept('lalala'));
 const rule3 = OneOrMoreRule.of(RawTextRule.of("la"));
 console.log(rule3.accept('lalalala'));
 
-const rule4 = AndRule.of(RawTextRule.of("hello"), RawTextRule.of(" world!"));
+const rule4 = AndRule.of([
+    RawTextRule.of("hello"),
+    RawTextRule.of(" world!")
+]);
 console.log(rule4.accept("hello world!"));
 
-const rule5 = AndRule.of(
-    OneOrMoreRule.of(RawTextRule.of('la')),
-    ZeroOrMoreRule.of(RawTextRule.of("fuck")),
-    OrRule.of(RawTextRule.of("!"), RawTextRule.of("?")));
-console.log(rule5.accept("lalalafuck?"));
+const rule5 = AndRule.of([
+    OneOrMoreRule.of(RawTextRule.of('la'), 'LA'),
+    ZeroOrMoreRule.of(RawTextRule.of("fuck"), "FUCK"),
+    OrRule.of([RawTextRule.of("!", "!!!"), RawTextRule.of("?", "WHAT")], "OR_SAMBLE")
+], 'AND_RULE');
+console.log(JSON.stringify(rule5.accept("lalalafuck?")));
